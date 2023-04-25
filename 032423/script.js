38becd081b5e822049809969f892fcf0fd1118fb
Doug Wright <wright-io@users.noreply.github.com>
2023-04-24 19:38:04 -0700

    Update script.js

--- 032423/script.js
+++ 032423/script.js
@@ -1,4 +1,5 @@
 function play() {
+  part.start(0);
   Tone.Transport.start();
 }
 
@@ -6,16 +7,11 @@ function stop() {
   Tone.Transport.stop();
 }
 
-function startSounds() {
-  var synth = new Tone.Synth().toDestination();
-  // use an array of objects as long as the object has a "time" attribute
-  var part = new Tone.Part(((time, value) => {
-    // the value is an object which contains both the note and the velocity
-    synth.triggerAttackRelease(value.note, "8n", time, value.velocity);
-  }), [{ time: 0, note: "C3", velocity: 0.9 },
-    { time: "0:2", note: "C4", velocity: 0.5 }
-  ]).start(0);
-}
-
-startSounds();
-  
+const synth = new Tone.Synth().toDestination();
+// use an array of objects as long as the object has a "time" attribute
+const part = new Tone.Part(((time, value) => {
+	// the value is an object which contains both the note and the velocity
+	synth.triggerAttackRelease(value.note, "8n", time, value.velocity);
+}), [{ time: 0, note: "C3", velocity: 0.9 },
+	{ time: "0:2", note: "C4", velocity: 0.5 }
+]).start(0);
