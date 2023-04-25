c9a4a8216f47c40e6039cbabacdfaac6a874dde0
Doug Wright <wright-io@users.noreply.github.com>
2023-04-24 19:33:55 -0700

    Add files via upload

---
+++ 032423/script.js
@@ -0,0 +1,21 @@
+function play() {
+  Tone.Transport.start();
+}
+
+function stop() {
+  Tone.Transport.stop();
+}
+
+function startSounds() {
+  var synth = new Tone.Synth().toDestination();
+  // use an array of objects as long as the object has a "time" attribute
+  var part = new Tone.Part(((time, value) => {
+    // the value is an object which contains both the note and the velocity
+    synth.triggerAttackRelease(value.note, "8n", time, value.velocity);
+  }), [{ time: 0, note: "C3", velocity: 0.9 },
+    { time: "0:2", note: "C4", velocity: 0.5 }
+  ]).start(0);
+}
+
+startSounds();
+  
